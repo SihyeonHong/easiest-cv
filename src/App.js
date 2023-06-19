8199d0dea4cece47b29514bbaa701f0ee143a157
SihyeonHong <hongsihyeon179@gmail.com>
2023-06-19 20:02:41 +0900

    [feat] home UI

--- src/App.js
+++ src/App.js
@@ -2,24 +2,42 @@ import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route, Link } from "react-router-dom";
+import { Nav, Container, Row, Col } from "react-bootstrap";
 
 function App() {
+  const userid = "userid".toUpperCase();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="mt-5 mb-5">{userid}</h1>
+      <Nav
+        variant="underline"
+        className="nav justify-content-center"
+        activeKey="/home"
+      >
+        <Nav.Item>
+          <Nav.Link href="/home">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">Tab1</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">Tab2</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <Container className="homeBody">
+        <Row>
+          <Col>
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                "/blank-profile-picture-973460_1920.png"
+              }
+              style={{ maxWidth: "640px", maxHeight: "1280px" }}
+            />
+          </Col>
+          <Col>자기소개 lorem ipsum </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
